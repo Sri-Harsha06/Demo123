a5688c120d0270140afcb2f3fce2308d9dbda0e7
Sai Harsha K V S S S R H <sriharsha.k@comakeit.com>
2022-09-09 16:07:06 +0530

    db insert

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Demo123.Models;
 namespace Demo123.Controllers;
-
+using System.Data.SqlClient;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
@@ -20,20 +20,22 @@ public class HomeController : Controller
     public IActionResult Addbook(Models.Book new_book)
     {
         // System.Console.WriteLine("hiii2222");
-        // List<Book> ls1 = new List<Book>();
-        // SqlConnection con = Database.Con;
-        // con.Open();
-        // SqlCommand cmd = new SqlCommand("select * from Book", con);
-        // System.Console.WriteLine("hii");
+        SqlConnection con = new SqlConnection("Server=localhost\\SQLEXPRESS;database=library;Trusted_Connection=false;Integrated Security = true;");
+        con.Open();
+        string query="insert into books(BookName,BookAuthor,NoOfCopies) output inserted.bookid values(@bookname,@bookAuthor,@copiesAvailable) ";
+        SqlCommand cmd = new SqlCommand(query, con);
+        cmd.Parameters.AddWithValue("@bookName",new_book.bookname);
+        cmd.Parameters.AddWithValue("@bookAuthor",new_book.bookauthor);
+        cmd.Parameters.AddWithValue("@copiesAvailable",new_book.noofcopiesavailable);
+        cmd.ExecuteNonQuery();
+        System.Console.WriteLine("hii");
         // SqlDataReader rd = cmd.ExecuteReader();
         // while (rd.Read())
         // {
         //     System.Console.WriteLine("{0}", rd[0]);
         //     ls1.Add(new Book { bookid = (int)rd[0], bookname = rd[1].ToString(), bookauthor = rd[2].ToString(), noofcopiesavailable = (int)rd[3] });
         // }
-        // con.Close();
-        ViewBag.id=new_book.bookid;
-        ViewBag.name=new_book.bookname;
+        con.Close();
         return View("Index");
     }
 
